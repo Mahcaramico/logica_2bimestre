521852eb1254268850e4822c8f96fab56c11a8a3
Mahcaramico caramico <marcella.caramico@eaportal.org>
2024-04-25 14:13:18 +0000

    exercicios pelo gitpode valores

---
+++ exercicio2.js
@@ -0,0 +1,16 @@
+const input = require ("prompt-sync") ()
+
+let codigo1= Number(input("digite o codigo da peça 1: "))
+let peças= Number(input("digite o numero de peças da peça 1: "))
+let valor= Number(input("digite o valor unitario da peça 1: "))
+
+let codigo2= Number(input("digite o codigo da peça 2: "))
+let peças2= Number(input("digite o numero de peças da peça 2: "))
+let valor2= Number(input("digite o valor unitario da peça 2: "))
+
+let total = (peças*valor)
+let total2 = (peças2*valor2)""
+let totaltotal = (total+total2)
+
+console.log (totaltotal)
+
